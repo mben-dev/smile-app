9b806b7ef6b31339606c118753d2c57c5cb39f18
mbenfriha <benfriha.mohamed@gmail.com>
2025-03-02 14:53:52 +0100

    update port for front

--- apps/api/start/env.ts
+++ apps/api/start/env.ts
@@ -28,7 +28,7 @@ export default await Env.create(new URL('../', import.meta.url), {
   |----------------------------------------------------------
   */
   DB_HOST: Env.schema.string({ format: 'host' }),
-  DB_PORT: Env.schema.number(),
+  DB_PORT: Env.schema.number.optional(),
   DB_USER: Env.schema.string(),
   DB_PASSWORD: Env.schema.string.optional(),
   DB_DATABASE: Env.schema.string(),
